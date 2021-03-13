b690308159f9ba3713126b182b266e1cb3e5c408
L1nu56 <69729571+L1nu56@users.noreply.github.com>
2021-03-13 18:09:07 +0100

    Update accesspoint.h
    Add classes and functions

--- os/accesspoint/accesspoint.h
+++ os/accesspoint/accesspoint.h
@@ -1,1 +1,41 @@
- 
+
+#ifndef Morse_h
+#define Morse_h
+
+// Start of class AP
+class AP {
+
+public:
+     AP(char[] ssid; char[] pass; short wpa);
+     
+     void start();
+     void stop();
+     void status();
+     
+private:
+     char[] _ssid;
+     char[] _pass;
+     short _wpa;
+     
+}
+// End of class AP
+
+
+// Start of class Server
+class Server {
+     
+public:
+     Server(double port);
+     
+     void printMenu();
+     void printMesg(void message);
+     
+     int getFunc();
+     
+private:
+     double _port;
+     
+}
+// End of class Server
+
+#endif
